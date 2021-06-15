7fe772a84191839aa47f72be7c27cbbe7faf6f35
VarisBranch <zaq0001234520@gmail.com>
2021-06-16 03:00:09 +0800

    3rd Version

--- GitHubSample/src/TestApp/mainApp.java
+++ GitHubSample/src/TestApp/mainApp.java
@@ -5,6 +5,8 @@ public class mainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("2nd Version");
+		//組員修改資料
+		System.out.println("3rd Version");
 	}
 
 }
